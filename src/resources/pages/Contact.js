5554e5966218b6456b86eb4e037db04d222040a5
Thusaraka Palliyeguru <87273240+thusarakap@users.noreply.github.com>
2024-02-12 01:16:24 +0530

    Create Contact.js

---
+++ src/resources/pages/Contact.js
@@ -0,0 +1,13 @@
+// Contact.js
+import React from 'react';
+
+const Contact = () => {
+  return (
+    <div>
+      <h1>Contact Us</h1>
+      <p>This is the Contact Us page content.</p>
+    </div>
+  );
+};
+
+export default Contact;
