3779651560ab0dda38b786e9e3dcc5441a3688ee
Thusaraka Palliyeguru <87273240+thusarakap@users.noreply.github.com>
2024-02-12 01:16:27 +0530

    Create Home.js

---
+++ src/resources/pages/Home.js
@@ -0,0 +1,28 @@
+// Home.js
+import React from 'react';
+
+const Home = () => {
+  return (
+    <div>
+      <h1>Welcome to Our Website!</h1>
+      <p>
+        We are dedicated to providing you with the best products and services.
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut blandit
+        sodales mauris nec fermentum. Integer ultricies dolor ac magna
+        sollicitudin vehicula.
+      </p>
+      <p>
+        <strong>Featured Products:</strong> Check out our latest offerings in
+        our <a href="/products">products</a> section.
+      </p>
+      <p>
+        Want to learn more <a href="/about">about us</a>? Feel free to explore!
+      </p>
+      <p>
+        Need assistance? Don't hesitate to <a href="/contact">contact us</a>.
+      </p>
+    </div>
+  );
+};
+
+export default Home;
