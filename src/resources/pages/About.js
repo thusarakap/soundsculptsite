72ac97885a4368d9038c938c13a63a6e79bf8d1d
Thusaraka Palliyeguru <87273240+thusarakap@users.noreply.github.com>
2024-02-12 01:16:20 +0530

    Create About.js

---
+++ src/resources/pages/About.js
@@ -0,0 +1,13 @@
+// About.js
+import React from 'react';
+
+const About = () => {
+  return (
+    <div>
+      <h1>About Us</h1>
+      <p>This is the About Us page content.</p>
+    </div>
+  );
+};
+
+export default About;
