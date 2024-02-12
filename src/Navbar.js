145f39a2fa295f957f1ad191d1fb87725a90f2ac
Thusaraka Palliyeguru <87273240+thusarakap@users.noreply.github.com>
2024-02-13 02:48:34 +0530

    Navbar Styling

--- src/Navbar.js
+++ src/Navbar.js
@@ -1,15 +1,28 @@
 // Navbar.js
 import React from 'react';
 import './resources/styles/navbarFooter.css';
+import emailIcon from './resources/images/emailIcon.png';
+import soundSculptLogo from './resources/images/soundSculptLogo.png';
 
 const Navbar = () => {
   return (
     <nav className="navbar">
+      <div className="logo">
+        <img src={soundSculptLogo} alt="Sound Sculpt Logo" />
+      </div>
       <ul>
-        <li><a href="/">Home</a></li>
+        
+        
         <li><a href="/about">About</a></li>
-        <li><a href="/contact">Contact</a></li>
+        <li><a href="/Concept">Pricing</a></li>
+        <li><a href="/">Home</a></li>
+        <li><a href="/Download">Download</a></li>
+        <li><a href="/Contact">Contact</a></li>
       </ul>
+      <div className="email">
+        <img src={emailIcon} alt="Email Icon" />
+        <a href="/Contact">contact@soundsculpt.com</a>
+        </div>
     </nav>
   );
 };
