b5878aa7592b648b82d12d8fc9e89451ee980f3d
Thusaraka Palliyeguru <87273240+thusarakap@users.noreply.github.com>
2024-02-25 01:47:20 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import Navbar from './Navbar';
 import Footer from './Footer';
 import Home from './resources/pages/Home';
 import About from './resources/pages/About';
+import Pricing from './resources/pages/Pricing';
 import Contact from './resources/pages/Contact';
 import Download from './resources/pages/Download';
 
@@ -15,6 +16,7 @@ function App() {
         <Routes>
           <Route path="soundsculptsite" element={<Home />} />
           <Route path="/about" element={<About />} />
+          <Route path="/Pricing" element={<Pricing />} />
           <Route path="/contact" element={<Contact />} />
           <Route path="/download" element={<Download />} />
         </Routes>
