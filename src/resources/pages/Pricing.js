53a1d79c5004de7d0bf842e334579025dd64bd7b
Thusaraka Palliyeguru <87273240+thusarakap@users.noreply.github.com>
2024-04-02 01:53:07 +0530

    Update Pricing.js

--- src/resources/pages/Pricing.js
+++ src/resources/pages/Pricing.js
@@ -5,35 +5,24 @@ import '../styles/pricing.css';
 const Pricing = () => {
   return (
     <div>
-      <h1>Pricing</h1>
+      <h1 className="pricing-heading">Pricing</h1>
       <div className="pricing-container">
         <div className="pricing-plan">
-          <h2>Basic</h2>
+          <h2>Free</h2>
           <ul>
             <li>Feature 1</li>
-            <li>Feature 2</li>
+            <li>8 presets</li>
             <li>Feature 3</li>
           </ul>
           <button>Choose Plan</button>
         </div>
         <div className="pricing-plan">
-          <h2>Standard</h2>
+          <h2>Supporter</h2>
           <ul>
             <li>Feature 1</li>
             <li>Feature 2</li>
             <li>Feature 3</li>
             <li>Feature 4</li>
-          </ul>
-          <button>Choose Plan</button>
-        </div>
-        <div className="pricing-plan">
-          <h2>Premium</h2>
-          <ul>
-            <li>Feature 1</li>
-            <li>Feature 2</li>
-            <li>Feature 3</li>
-            <li>Feature 4</li>
-            <li>Feature 5</li>
           </ul>
           <button>Choose Plan</button>
         </div>
